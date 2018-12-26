3c65266b84b9d4dc47ab950efd2095aff63e9282
Rajat Singh <rajatsingh9bdpsalld@gmail.com>
2018-12-26 16:39:15 +0530

    userStore test

--- src/app/services/login.service.spec.ts
+++ src/app/services/login.service.spec.ts
@@ -7,26 +7,32 @@ import { HttpClient, HttpHandler } from '@angular/common/http';
 import { Router } from '@angular/router';
 
 describe('LoginService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
+  beforeEach(() => {
+     TestBed.configureTestingModule({
     imports: [
       HttpClientTestingModule
     ],
     providers: [
       AuthenticationService,
       Broadcaster,
-      { provide: AUTH_API_URL, useValue: 'https://auth.prod-preview.openshift.io/api/' },
-      { provide: SSO_API_URL, useValue: 'https://sso.prod-preview.openshift.io/api/' },
-      { provide: WIT_API_PROXY, useValue: 'https://prod-preview.openshift.io/api/' },
+      { provide: AUTH_API_URL, useValue: 'https://auth.example.com' },
+      { provide: SSO_API_URL, useValue: 'https://sso.example.com' },
+      { provide: WIT_API_PROXY, useValue: 'https://wit.example.com' },
       { provide: REALM, useValue: 'realm' },
     HttpClient,
     HttpHandler,
     {provide: Router}
     ]
-  }));
-  it('should return URL', ( ) => {});
+  });
+
+});
 
   it('should be created', () => {
     const service: LoginService = TestBed.get(LoginService);
     expect(service).toBeTruthy();
+  });
+  it('should redirect to Auth', () => {
+    const service: LoginService = TestBed.get(LoginService);
+    expect(service.redirectToAuth).toBe('test');
   });
 });
